567c730d2b580622f61deae5361dca85e7b62cba
adrianc2211 <adrianc2211@gmail.com>
2022-06-28 20:09:22 +0200

    Utworzenie klas

---
+++ Magazyn - Rozmieszczenie Towarów/Models/towary.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magazyn___Rozmieszczenie_Towarów.Models
+{
+    internal class towary
+    {
+    }
+}
