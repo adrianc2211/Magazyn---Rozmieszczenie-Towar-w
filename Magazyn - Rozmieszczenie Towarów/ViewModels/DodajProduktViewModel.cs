4cae87ac862827a2bead833ac0b6ecc17690ddf8
adrianc2211 <adrianc2211@gmail.com>
2022-06-30 11:27:27 +0200

    DodajProduktVM - utworzenie

---
+++ Magazyn - Rozmieszczenie Towarów/ViewModels/DodajProduktViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magazyn___Rozmieszczenie_Towarów.ViewModels
+{
+    internal class DodajProduktViewModel:ViewModelBase
+    {
+         
+    }
+}
