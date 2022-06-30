a709340c4bf43f46aa33850933242132aba8c7c9
adrianc2211 <adrianc2211@gmail.com>
2022-06-30 11:42:26 +0200

    DodajProduktVM -done

--- Magazyn - Rozmieszczenie Towarów/ViewModels/DodajProduktViewModel.cs
+++ Magazyn - Rozmieszczenie Towarów/ViewModels/DodajProduktViewModel.cs
@@ -3,11 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Magazyn___Rozmieszczenie_Towarów.ViewModels
 {
-    internal class DodajProduktViewModel:ViewModelBase
+    internal class DodajProduktViewModel : ViewModelBase
     {
-         
-    }
+        private int _IdTowaru;
+        public int IdTowaru
+        {
+            get { return _IdTowaru; }
+            set
+            {
+                _IdTowaru = value;
+                OnPropertyChange(nameof(IdTowaru));
+            }
+        }
+
+        private string _NazwaTowaru;
+        public string NazwaTowaru
+        {
+            get { return _NazwaTowaru; }
+            set
+            {
+                _NazwaTowaru = value;
+                OnPropertyChange(nameof(NazwaTowaru));
+            }
+        }
+
+        private string _WagaTowaru;
+        public string WagaTowaru
+        {
+            get { return _WagaTowaru; }
+            set
+            {
+                _WagaTowaru = value;
+                OnPropertyChange(nameof(WagaTowaru));
+            }
+        }
+
+        public ICommand DodajCommand { get; }
+        public ICommand CancellCommand { get; }
+}
 }
