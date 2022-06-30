fac77fcd3a7ed63f61ddaf3dcef8aef46a65ba0a
adrianc2211 <adrianc2211@gmail.com>
2022-06-30 11:26:37 +0200

    ViewModelBase

---
+++ Magazyn - Rozmieszczenie Towarów/ViewModels/ViewModelBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magazyn___Rozmieszczenie_Towarów.ViewModels
+{
+    internal class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChange(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName)); 
+        }
+    }
+}
