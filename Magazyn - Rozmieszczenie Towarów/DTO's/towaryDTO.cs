24b85637f9f50c62333a5bc0bc16528c26590790
adrianc2211 <adrianc2211@gmail.com>
2022-07-01 12:44:09 +0200

    connectionString

---
+++ Magazyn - Rozmieszczenie Towarów/DTO's/towaryDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magazyn___Rozmieszczenie_Towarów.DTO_s
+{
+    public class towaryDTO
+    {
+        [Key]
+        public Guid IdTowaru { get; set; }
+        public string nazwa_towaru { get; set; }
+        public int waga { get; set; }
+    }
+}
---
+++ Magazyn - Rozmieszczenie Towarów/DbContexts/MagazynDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Magazyn___Rozmieszczenie_Towarów.DTO_s;
+using Microsoft.EntityFrameworkCore;
+
+namespace Magazyn___Rozmieszczenie_Towarów.DbContexts
+{
+    public class MagazynDbContext:DbContext
+    {
+        //DTO- data transfer object - przenosi dane między bazą danych a aplikacją
+        public DbSet<towaryDTO> Towary { get; set; }
+        
+    }
+}
