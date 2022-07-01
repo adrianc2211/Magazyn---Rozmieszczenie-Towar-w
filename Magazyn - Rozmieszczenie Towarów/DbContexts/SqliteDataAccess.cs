612144ec25f8257f395ea2f4086fb8450d078760
adrianc2211 <adrianc2211@gmail.com>
2022-07-01 14:54:05 +0200

    sqlite data access

---
+++ Magazyn - Rozmieszczenie Towarów/DbContexts/SqliteDataAccess.cs
@@ -0,0 +1,38 @@
+﻿using Dapper;
+using Magazyn___Rozmieszczenie_Towarów.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magazyn___Rozmieszczenie_Towarów.DbContexts
+{
+    public class SqliteDataAccess
+    {
+        public static List<towary> LoadTowary()
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                var output = cnn.Query<towary>("select * from towary", new DynamicParameters());
+                return output.ToList();
+            }
+        }
+
+        public static void DodajTowar(towary towar)
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                cnn.Execute("insert into towary(id_towaru, nazwa_towaru, waga values @IdTowaru, @NazwaTowaru, @waga)", towar);
+            }
+        }
+
+        public static string LoadConnectionString(string id = "Default")
+        {
+            return ConfigurationManager.ConnectionStrings[id].ConnectionString;
+        }
+    }
+}
